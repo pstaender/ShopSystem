29f222d337a55134b01e1b75b18e5e10096eb775
Philipp Ständer <philipp.staender@gmail.com>
2010-11-05 18:41:28 +0100

    define, when is out of stock

--- code/ShopCart.php
+++ code/ShopCart.php
@@ -13,6 +13,7 @@ class ShopCart_Controller extends ShopController {
 	function add() {
 		if ($id = Director::urlParam("ID")) {
 			$quantity = (Director::urlParam("OtherID")) ? (int) Director::urlParam("OtherID") : 1;
+			if ($item=DataObject::get_by_id("ShopItem",$id)) if ($item->StockQuantity>=0) if ($item->StockQuantity-$quantity<0) exit(_t("Shop.OutOfStock","%Out Of Stock%")); 
 			$optionID = (isset($_REQUEST['optionid'])) ? (int) $_REQUEST['optionid'] : null;
 			if (ShopOrder::addItem((int) $id, $quantity, $optionID)) {
 				$this->Message = "OK";
